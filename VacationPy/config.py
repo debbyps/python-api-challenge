4e09f7c97576a93dca2d9e601da55805ace4d25d
Debby Pulido <66884955+debbyps@users.noreply.github.com>
2020-08-05 23:35:02 -0700

    adding homework

---
+++ VacationPy/config.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b7e6a0a6f04208a3fef57063bebafc34 "
+
+# Google API Key
+g_key = "AIzaSyAWFJjo3u5Kws_MYA2O_MEGOf3aJVDbpxg"
--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b7e6a0a6f04208a3fef57063bebafc34"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
